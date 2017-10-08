f1cb753331209caa0463224816dde9639f5ecb52
James Edward Gray II <james@graysoftinc.com>
2017-10-08 11:52:52 -0500

    View fixes.

--- lib/photobooth_web/controllers/home_controller.ex
+++ lib/photobooth_web/controllers/home_controller.ex
@@ -4,7 +4,7 @@ defmodule PhotoboothWeb.HomeController do
   def index(conn, _params) do
     Picam.set_img_effect(:none)
     Picam.set_size(640, 0)
-    Picam.set_rotation(270)
+    # Picam.set_rotation(270)
 
     render(conn, "index.html")
   end
