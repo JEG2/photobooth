79ac83f146ed9037fb177f5a982eb82e51c4cfc6
James Edward Gray II <james@graysoftinc.com>
2017-10-12 19:57:42 -0500

    Adding region.

--- config/config.exs
+++ config/config.exs
@@ -18,6 +18,9 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :ex_aws,
+  s3: [region: "us-west-2"]
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
